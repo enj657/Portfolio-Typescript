a1e0ba09ab92006649449010254f81384f65aab6
Elle Jacobs <enj657@gmail.com>
2025-03-27 16:04:47 -0500

    Pokemon data update

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -292,7 +292,7 @@ export const PersonalProjects = (): PersonalProjectsData => {
       href: 'https://pokedex-enj.vercel.app/',
       website: 'Pokedex',
       publishDate: 'March 2025',
-      listItems: ['React', 'JavaScript', 'HTML', 'CSS', 'Redux','vite', 'REST API'],
+      listItems: ['React', 'JavaScript', 'HTML', 'CSS', 'Redux','vite', 'REST API', 'react-query'],
     },
   ];
 
