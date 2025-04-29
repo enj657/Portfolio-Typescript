fc182e8cfd72ce64893ef4c4bbf19353913fcc90
Elle Jacobs <enj657@gmail.com>
2025-04-29 13:02:29 -0500

    More SE wording update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,7 +20,7 @@ const AboutComponent: React.FC<AboutProps> = ({
         >
           About
         </h2>
-        <p className='mb-6 mt-4 text-center text-base md:text-left'>{`Hey, I'm Elle, a web developer who loves creating fun websites. I got hooked on web development after a data analytics bootcamp in 2022.`}</p>
+        <p className='mb-6 mt-4 text-center text-base md:text-left'>{`Hey, I'm Elle, a software engineer who loves creating fun websites. I got hooked on software engineering after a data analytics bootcamp in 2022.`}</p>
         <p className='mb-6 text-center text-base md:text-left'>{`I'm all about making websites look incredible on any device and accessible to everyone. I want the web to be a fun and inclusive place for all. I love keeping up to date with the latest technologies and have been continuously improving my knowledge of HTML, JS, and CSS.`}</p>
         <p className='mb-6 text-center text-base md:text-left'>{`When I'm not coding, you can find me hiking, hanging out with loved ones, playing music, or exploring virtual worlds in video games.`}</p>
       </div>
